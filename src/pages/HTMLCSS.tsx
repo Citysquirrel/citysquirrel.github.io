586c6e3f0f0868e65c2552f5905b6309cd79cc4f
Citysquirrel <86960007+Citysquirrel@users.noreply.github.com>
2022-03-16 20:32:49 +0900

    Add tab menus

--- src/pages/HTMLCSS.tsx
+++ src/pages/HTMLCSS.tsx
@@ -14,9 +14,34 @@ const Intro = styled.div`
 `;
 
 const Menu = styled.div`
-  display: flex;
-  > div {
-    flex: auto;
+  /* display: flex; */
+  background-color: var(--bg-menu);
+  > .menu-wrapper {
+    max-width: 968px;
+    display: flex;
+    /* flex: auto; */
+    margin: 8px;
+    margin-left: auto;
+    margin-right: auto;
+    background-color: inherit;
+    overflow-x: auto;
+    ::-webkit-scrollbar {
+      display: none;
+    }
+    > .menu-tab {
+      flex: 1 0 128px;
+      /* display: inline-block; */
+      margin: 0 4px;
+      /* width: 128px; */
+      background-color: inherit;
+      border-bottom: 2px solid var(--bg-menu);
+      cursor: pointer;
+      :hover {
+        background-color: #9dc3f3;
+        border-bottom: 2px solid #5d9cec;
+        transition: all 0.3s;
+      }
+    }
   }
 `;
 
@@ -25,11 +50,20 @@ const Contents = styled.div``;
 export const HTMLCSS = () => {
   return (
     <Container>
-      <Intro>ㅎㅇ</Intro>
+      <Intro>Introduction</Intro>
       <Menu>
-        <div>1</div>
-        <div>2</div>
-        <div>3</div>
+        <div className="menu-wrapper">
+          <div className="menu-tab">1</div>
+          <div className="menu-tab">2</div>
+          <div className="menu-tab">3</div>
+          <div className="menu-tab">4</div>
+          <div className="menu-tab">5</div>
+          <div className="menu-tab">6</div>
+          <div className="menu-tab">7</div>
+          <div className="menu-tab">8</div>
+          <div className="menu-tab">9</div>
+          <div className="menu-tab">10</div>
+        </div>
       </Menu>
       <Contents></Contents>
     </Container>
