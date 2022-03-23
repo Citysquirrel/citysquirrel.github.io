e63bd281c189af29d1ce0b7ac36c5a7b8b92e3fa
Citysquirrel <86960007+Citysquirrel@users.noreply.github.com>
2022-03-23 15:36:44 +0900

    Change page contents

--- src/pages/Examples.tsx
+++ src/pages/Examples.tsx
@@ -9,10 +9,31 @@ const Container = styled.div`
 
 const Intro = styled.div`
   padding-top: 64px;
-  height: 256px;
+  padding-bottom: 16px;
+  min-height: 256px;
   /* max-width: 968px; */
   background-color: var(--bg-menu);
   font-family: 'SUIT-Light';
+  > h1 {
+    margin: 8px auto;
+    font-size: 48px;
+    width: fit-content;
+    border-bottom: 2px solid #9dc3f3;
+    margin-bottom: 8px;
+    background-color: inherit;
+  }
+  > div {
+    margin-left: auto;
+    margin-right: auto;
+    line-height: 40px;
+    max-width: 768px;
+    white-space: pre-wrap;
+    word-break: keep-all;
+    background-color: inherit;
+    > a {
+      background-color: #9dc3f3;
+    }
+  }
 `;
 
 const Menu = styled.div`
@@ -58,8 +79,28 @@ export const Examples = () => {
             path="/"
             element={
               <>
-                아직 공사중이예요 ㅠ.. 페이지 어떻게 구성할지 좀 더 고민후에
-                찾아뵐게요!
+                <h1>WHAT?</h1>
+                <div>
+                  <a
+                    href="https://velog.io/@tok1324"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    개인 블로그
+                  </a>
+                  에 작성되는 예제들을 모아놓은 페이지입니다.
+                  <p /> 웹 페이지를 구성하기 위해 필요한 요소들에 대한 개념과 그
+                  예제를 정리했습니다!
+                </div>
+                <h1>WHY?</h1>
+                <div>
+                  개발자로서 뭔가 배울 때 어떤 코드에 대한 예제가 있으면 굉장히
+                  큰 도움이 됩니다. <p />
+                  다른 사람들은 이 코드를 어떻게 짰지? 하는 의문이 생길 때가
+                  다들 있을겁니다. <p />나 자신 뿐만 아니라, 주니어 및 시니어
+                  개발자 분들에게도 보고 참고가 될만한 예제들을 정리하는 것이 이
+                  페이지의 목적입니다 :)
+                </div>
               </>
             }
           />
