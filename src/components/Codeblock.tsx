fe71ab708e5d532200339bccdffd1c3799b94a27
Citysquirrel <86960007+Citysquirrel@users.noreply.github.com>
2022-03-25 20:29:38 +0900

    Create Codeblock component

--- src/components/Codeblock.tsx
+++ src/components/Codeblock.tsx
@@ -1,10 +1,54 @@
 import styled from 'styled-components';
+import { FaRegCopy } from 'react-icons/fa';
+import './../index.css';
+import { useState } from 'react';
 
-const Container = styled.div``;
+const Container = styled.div`
+  position: relative;
+  border-radius: 8px;
+  margin: 8px;
+  background-color: transparent;
+  /* border: 1px solid grey; */
+  > pre {
+    > code {
+      text-align: left;
+      font-size: 14px;
+      border-radius: 8px;
+      > span {
+        font-size: inherit;
+      }
+    }
+  }
+`;
 
-const CopyWrapper = styled.div``;
+const CopyWrapper = styled.div<{ visibility: boolean }>`
+  position: absolute;
+  top: 0;
+  right: 0;
+  width: 32px;
+  height: 32px;
+  margin: 8px;
+  border-radius: 4px;
+  border: 1px solid var(--main);
+  background-color: var(--main-tint);
+  visibility: ${(props) => (props.visibility ? 'visible' : 'hidden')};
+  opacity: ${(props) => (props.visibility ? '1' : '0')};
+  transition: 0.3s;
+  cursor: pointer;
+  :hover {
+    border-color: aliceblue;
+    background-color: var(--main-tint-more);
+  }
+`;
 
-const CopyButton = styled.div``;
+const CopyButton = styled.div`
+  background-color: transparent;
+
+  > svg {
+    font-size: 20px;
+    background-color: inherit;
+  }
+`;
 
 interface Props {
   body: string;
@@ -13,11 +57,28 @@ interface Props {
 // props로 본문, 언어 받아서 사용
 // 그리고 상위 컴포넌트에서 highlight.js 실행
 export const Codeblock = ({ body, lang }: Props) => {
+  const [copyBtn, setCopyBtn] = useState(false);
+
+  const handleMouseOver = () => {
+    setCopyBtn(true);
+  };
+  const handleMouseBlur = () => {
+    setCopyBtn(false);
+  };
+  const handleCopy = () => {
+    navigator.clipboard.writeText(body);
+  };
+
   return (
-    <Container>
+    <Container onMouseOver={handleMouseOver} onMouseOut={handleMouseBlur}>
       <pre>
         <code className={`language-${lang}`}>{body}</code>
       </pre>
+      <CopyWrapper visibility={copyBtn} onClick={handleCopy}>
+        <CopyButton>
+          <FaRegCopy />
+        </CopyButton>
+      </CopyWrapper>
     </Container>
   );
 };
--- src/pages/study/Javascript.tsx
+++ src/pages/study/Javascript.tsx
@@ -1,17 +1,28 @@
 import { useEffect } from 'react';
+import styled from 'styled-components';
 import hljs from 'highlight.js';
+import { Codeblock } from '../../components';
+
+const Container = styled.div``;
+
+const Wrapper = styled.div`
+  max-width: 968px;
+  margin-left: auto;
+  margin-right: auto;
+`;
 
 export const Javascript = () => {
   useEffect(() => {
     hljs.highlightAll();
   }, []);
   return (
-    <>
-      <pre>
-        <code className="language-javascript">
-          {`const num = 0; \n console.log(num);`}
-        </code>
-      </pre>
-    </>
+    <Container>
+      <Wrapper>
+        <Codeblock
+          body={`const num = 0; \nconsole.log(num);`}
+          lang="javascript"
+        />
+      </Wrapper>
+    </Container>
   );
 };
