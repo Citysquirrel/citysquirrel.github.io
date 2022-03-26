6054d98719079d148c1707e7cd76259ab55d1d50
Citysquirrel <86960007+Citysquirrel@users.noreply.github.com>
2022-03-26 15:57:22 +0900

    Set button default css & Write usages of functional components

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,3 +1,23 @@
 import { Codeblock } from './Codeblock';
 import { Modal } from './Modal';
+
 export { Codeblock, Modal };
+
+// Modal props: { width, height, element, modal, setModal }
+// ? Usage example
+// ! Modal Component
+// <Modal
+//   width="500"
+//   height="300"
+//   element={<div>Example!</div>}
+//   modal={modalState}
+//   setModal={SetModalState Function}
+// />
+//
+// ! Codeblock Component
+// <Codeblock
+//           body={`const num = 0;
+// console.log(num);
+// const test = test;`}
+//           lang="javascript"
+//         />
