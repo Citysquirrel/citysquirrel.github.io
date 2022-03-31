78331c1e770ef0a50a2cef583dbd9f545d280e7d
Citysquirrel <86960007+Citysquirrel@users.noreply.github.com>
2022-03-31 11:57:27 +0900

    Add theme provider

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,11 @@ import './App.css';
 import { Footer, Header } from './layouts';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { Home, Examples, Pictures, Wrong, Study } from './pages';
-import styled from 'styled-components';
+import styled, { ThemeProvider } from 'styled-components';
 
-const Container = styled.div``;
+const Container = styled.div`
+  border: 2px solid ${(props) => props.theme.bg};
+`;
 
 const Pages = styled.div`
   max-width: 968px;
@@ -12,23 +14,42 @@ const Pages = styled.div`
   margin-right: auto;
 `;
 
+// --bg-menu: #e1ecfb;
+// --bg-menu-tp: rgba(225, 236, 251, 0.61);
+// --bg: #edf4fc;
+// --main-dark: #355986;
+// --main: #5d9cec;
+// --main-tint: #9dc3f3;
+// --main-tint-more: #c7dcf8;
+const light_theme = {
+  bg: '#edf4fc',
+  bg_menu: '#e1ecfb',
+  bg_menu_tp: 'rgba(255,236,251,0.61)',
+  main: '#5d9cec',
+  main_dark: '#355986',
+  main_tint: '#9dc3f3',
+  main_tint_more: '#c7dcf8',
+};
+
 function App() {
   return (
-    <Router>
-      <div className="App">
-        <Header />
-        <Container id="container">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            {/* <Route path="/examples/*" element={<Examples />} /> */}
-            <Route path="/pictures/*" element={<Pictures />} />
-            {/* <Route path="/study/*" element={<Study />} /> */}
-            <Route path="/*" element={<Wrong />} />
-          </Routes>
-          <Footer />
-        </Container>
-      </div>
-    </Router>
+    <ThemeProvider theme={light_theme}>
+      <Router>
+        <div className="App">
+          <Header />
+          <Container id="container">
+            <Routes>
+              <Route path="/" element={<Home />} />
+              {/* <Route path="/examples/*" element={<Examples />} /> */}
+              <Route path="/pictures/*" element={<Pictures />} />
+              {/* <Route path="/study/*" element={<Study />} /> */}
+              <Route path="/*" element={<Wrong />} />
+            </Routes>
+            <Footer />
+          </Container>
+        </div>
+      </Router>
+    </ThemeProvider>
   );
 }
 
