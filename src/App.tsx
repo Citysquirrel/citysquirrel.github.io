64356f8798e056c0682f163d69af93811486fc6c
Citysquirrel <86960007+Citysquirrel@users.noreply.github.com>
2022-04-24 12:47:56 +0900

    how to change color theme of pages

--- src/App.tsx
+++ src/App.tsx
@@ -45,6 +45,7 @@ function App() {
 
   useEffect(() => {
     document.documentElement.setAttribute('color-theme', colorTheme || 'light');
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   return (
