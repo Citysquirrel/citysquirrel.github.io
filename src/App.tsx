fc0dc258a2a23d563b52e6fff0dea6cf22279455
Citysquirrel <86960007+Citysquirrel@users.noreply.github.com>
2022-08-30 21:50:37 +0900

    feat: devide JSX structure of App.js

--- src/App.tsx
+++ src/App.tsx
@@ -8,9 +8,9 @@ import {
   useLocation,
   useParams,
 } from 'react-router-dom';
-import styled, { ThemeProvider } from 'styled-components';
+import styled from 'styled-components';
 import { Blog, Home } from './pages';
-import { ScrollToTop } from './components/wrappers';
+import { MasterProvider, ScrollToTop } from './components/wrappers';
 
 const Container = styled.div`
   display: flex;
@@ -24,16 +24,6 @@ const Container = styled.div`
     width: 80vw;
   }
 `;
-
-const light_theme = {
-  bg: '#edf4fc',
-  bg_menu: '#e1ecfb',
-  bg_menu_tp: 'rgba(255,236,251,0.61)',
-  main: '#5d9cec',
-  main_dark: '#355986',
-  main_tint: '#9dc3f3',
-  main_tint_more: '#c7dcf8',
-};
 
 interface IRouterContext {
   number: string | undefined;
@@ -55,37 +45,31 @@ function App() {
   }, []);
 
   return (
-    // <RouterContext.Provider value={routerContext}>
-    <ThemeProvider theme={light_theme}>
-      <Router basename="/">
-        <ScrollToTop>
-          <>
-            <div id="top" className="entire-border"></div>
-            <div id="right" className="entire-border"></div>
-            <div id="bottom" className="entire-border"></div>
-            <div id="left" className="entire-border"></div>
-            <div className="App">
-              <Header />
+    <MasterProvider>
+      <>
+        <div id="top" className="entire-border"></div>
+        <div id="right" className="entire-border"></div>
+        <div id="bottom" className="entire-border"></div>
+        <div id="left" className="entire-border"></div>
+        <div className="App">
+          <Header />
 
-              <Container id="container">
-                <Routes>
-                  <Route path="/*" element={<Home />} />
-                  <Route path="/blog/" element={<Blog />} />
-                  <Route path="/blog/:number" element={<Blog />} />
+          <Container id="container">
+            <Routes>
+              <Route path="/*" element={<Home />} />
+              <Route path="/blog/" element={<Blog />} />
+              <Route path="/blog/:number" element={<Blog />} />
 
-                  {/* <Route path="/examples/*" element={<Examples />} /> */}
-                  {/* <Route path="/pictures/*" element={<Pictures />} /> */}
-                  {/* <Route path="/study/*" element={<Study />} /> */}
-                  {/* <Route path="/*" element={<Wrong />} /> */}
-                </Routes>
-                <Footer />
-              </Container>
-            </div>
-          </>
-        </ScrollToTop>
-      </Router>
-    </ThemeProvider>
-    // </RouterContext.Provider>
+              {/* <Route path="/examples/*" element={<Examples />} /> */}
+              {/* <Route path="/pictures/*" element={<Pictures />} /> */}
+              {/* <Route path="/study/*" element={<Study />} /> */}
+              {/* <Route path="/*" element={<Wrong />} /> */}
+            </Routes>
+            <Footer />
+          </Container>
+        </div>
+      </>
+    </MasterProvider>
   );
 }
 
--- src/components/wrappers/index.tsx
+++ src/components/wrappers/index.tsx
@@ -1,5 +1,7 @@
 import { useEffect } from 'react';
 import { useLocation } from 'react-router';
+import { BrowserRouter } from 'react-router-dom';
+import { ThemeProvider } from 'styled-components';
 
 export const ScrollToTop = (props: { children: JSX.Element }) => {
   const location = useLocation();
@@ -10,6 +12,27 @@ export const ScrollToTop = (props: { children: JSX.Element }) => {
   return <>{props.children}</>;
 };
 
+/**
+ * @description BroswerRouter를 통해 basename을 지정합니다. 또한 갖가지 Provider들을 본 함수에서 조작합니다.
+ * 자식 컴포넌트는 반드시 하나의 최상위 엘레먼트를 가져야 합니다.
+ * @param props JSX Element children
+ * @returns BrowserRouter, Providers을 포함한 JSX Element
+ */
 export const MasterProvider = (props: { children: JSX.Element }) => {
-  return <>{props.children}</>;
+  const themeInfos = {
+    bg: '#edf4fc',
+    bg_menu: '#e1ecfb',
+    bg_menu_tp: 'rgba(255,236,251,0.61)',
+    main: '#5d9cec',
+    main_dark: '#355986',
+    main_tint: '#9dc3f3',
+    main_tint_more: '#c7dcf8',
+  };
+  return (
+    <ThemeProvider theme={themeInfos}>
+      <BrowserRouter basename="/">
+        <ScrollToTop>{props.children}</ScrollToTop>
+      </BrowserRouter>
+    </ThemeProvider>
+  );
 };
