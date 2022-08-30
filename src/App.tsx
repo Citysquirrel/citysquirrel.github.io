09a9f1f1d9273d86f02c806119486422c59e19ba
Citysquirrel <86960007+Citysquirrel@users.noreply.github.com>
2022-08-30 19:44:10 +0900

    feat: set context api for router context(params, location now)

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
 import './App.css';
 import { createContext, useMemo, useState, Dispatch, useEffect } from 'react';
 import { Footer, Header } from './layouts';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  useLocation,
+  useParams,
+} from 'react-router-dom';
 import styled, { ThemeProvider } from 'styled-components';
 import { Blog, Home } from './pages';
 import { ScrollToTop } from './components/wrappers';
@@ -29,14 +35,25 @@ const light_theme = {
   main_tint_more: '#c7dcf8',
 };
 
+interface IRouterContext {
+  number: string | undefined;
+  search: string | undefined;
+}
 type TestDispatch = Dispatch<React.SetStateAction<string>>;
-export const TestContext = createContext<string | null>('힝');
-export const TestDispatchContext = createContext<TestDispatch | null>(null);
+export const RouterContext = createContext<IRouterContext>({
+  number: undefined,
+  search: undefined,
+});
+// export const TestDispatchContext = createContext<TestDispatch | null>(null);
 
 function App() {
-  const [globalTest, setGlobalTest] = useState('전역상태 만들어보기');
-  const testMemo = useMemo(() => globalTest, [globalTest]);
-  const dispatchMemo = useMemo(() => setGlobalTest, [setGlobalTest]);
+  // const [globalTest, setGlobalTest] = useState('전역상태 만들어보기');
+  const { search } = useLocation();
+  const { number } = useParams();
+  const routerContext = useMemo(() => {
+    return { number, search };
+  }, [number, search]);
+  // const dispatchMemo = useMemo(() => setGlobalTest, [setGlobalTest]);
 
   const colorTheme = localStorage.getItem('colorTheme');
 
@@ -47,39 +64,39 @@ function App() {
   }, []);
 
   return (
-    <TestContext.Provider value={testMemo}>
-      <TestDispatchContext.Provider value={dispatchMemo}>
-        <ThemeProvider theme={light_theme}>
-          <Router basename="/">
-            <ScrollToTop>
-              <>
-                <div id="top" className="entire-border"></div>
-                <div id="right" className="entire-border"></div>
-                <div id="bottom" className="entire-border"></div>
-                <div id="left" className="entire-border"></div>
-                <div className="App">
-                  <Header />
+    <RouterContext.Provider value={routerContext}>
+      {/* <TestDispatchContext.Provider value={dispatchMemo}> */}
+      <ThemeProvider theme={light_theme}>
+        <Router basename="/">
+          <ScrollToTop>
+            <>
+              <div id="top" className="entire-border"></div>
+              <div id="right" className="entire-border"></div>
+              <div id="bottom" className="entire-border"></div>
+              <div id="left" className="entire-border"></div>
+              <div className="App">
+                <Header />
 
-                  <Container id="container">
-                    <Routes>
-                      <Route path="/*" element={<Home />} />
-                      <Route path="/blog/" element={<Blog />} />
-                      <Route path="/blog/:number" element={<Blog />} />
+                <Container id="container">
+                  <Routes>
+                    <Route path="/*" element={<Home />} />
+                    <Route path="/blog/" element={<Blog />} />
+                    <Route path="/blog/:number" element={<Blog />} />
 
-                      {/* <Route path="/examples/*" element={<Examples />} /> */}
-                      {/* <Route path="/pictures/*" element={<Pictures />} /> */}
-                      {/* <Route path="/study/*" element={<Study />} /> */}
-                      {/* <Route path="/*" element={<Wrong />} /> */}
-                    </Routes>
-                    <Footer />
-                  </Container>
-                </div>
-              </>
-            </ScrollToTop>
-          </Router>
-        </ThemeProvider>
-      </TestDispatchContext.Provider>
-    </TestContext.Provider>
+                    {/* <Route path="/examples/*" element={<Examples />} /> */}
+                    {/* <Route path="/pictures/*" element={<Pictures />} /> */}
+                    {/* <Route path="/study/*" element={<Study />} /> */}
+                    {/* <Route path="/*" element={<Wrong />} /> */}
+                  </Routes>
+                  <Footer />
+                </Container>
+              </div>
+            </>
+          </ScrollToTop>
+        </Router>
+      </ThemeProvider>
+      {/* </TestDispatchContext.Provider> */}
+    </RouterContext.Provider>
   );
 }
 
