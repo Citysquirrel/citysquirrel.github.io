6c6ac23e37763a4a8eafe4f813bd021ff48cd195
Citysquirrel <tok1324@naver.com>
2024-07-16 01:38:36 +0900

    미세설정 및 조건문 변경

--- src/functions/index.tsx
+++ src/functions/index.tsx
@@ -1,12 +1,12 @@
-import { useEffect } from 'react';
-import { Octokit } from 'octokit';
-import { FaRegCopy } from 'react-icons/fa';
-import styled from 'styled-components';
+import { useEffect } from "react";
+import { Octokit } from "octokit";
+import { FaRegCopy } from "react-icons/fa";
+import styled from "styled-components";
 
 export function useFadeIn() {
-  useEffect(() => {
-    console.log(123);
-  }, []);
+	useEffect(() => {
+		console.log(123);
+	}, []);
 }
 
 /**
@@ -15,17 +15,17 @@ export function useFadeIn() {
  * @returns `boolean` (ex: `true`)
  */
 export function isLightColorTone(color: string) {
-  let code = '';
-  if (color[0] === '#') code = color.slice(1);
-  else code = color.slice();
-  const firstLetter = parseInt(color[1], 16);
-  const red = code.slice(1, 2);
-  const green = code.slice(3, 4);
-  const blue = code.slice(5, 6);
-  //! R, G, B에 따라서 나눌 필요가 있음. 현재는 임시방편
-  // 0 1 2 3 4 5 6 7 => dark tone
-  // 8 9 A B C D E F => light tone
-  return firstLetter > 8;
+	let code = "";
+	if (color[0] === "#") code = color.slice(1);
+	else code = color.slice();
+	const firstLetter = parseInt(color[1], 16);
+	const red = code.slice(1, 2);
+	const green = code.slice(3, 4);
+	const blue = code.slice(5, 6);
+	//! R, G, B에 따라서 나눌 필요가 있음. 현재는 임시방편
+	// 0 1 2 3 4 5 6 7 => dark tone
+	// 8 9 A B C D E F => light tone
+	return firstLetter > 8;
 }
 
 /**
@@ -34,91 +34,91 @@ export function isLightColorTone(color: string) {
  * @param color 변경될 배경색
  */
 export function useScreenBorder(
-  options: {
-    top: string;
-    right: string;
-    bottom: string;
-    left: string;
-    container: string;
-  },
-  color: string = '#c4e0fa'
+	options: {
+		top: string;
+		right: string;
+		bottom: string;
+		left: string;
+		container: string;
+	},
+	color: string = "#c4e0fa"
 ) {
-  useEffect(() => {
-    const top = document.getElementById('top');
-    const right = document.getElementById('right');
-    const bottom = document.getElementById('bottom');
-    const left = document.getElementById('left');
-    const container = document.getElementById('container');
-    const footer = document.getElementById('footer');
-    const footerCopyright = document.getElementById('footer-copyright');
-    if (top !== null) {
-      top.style.height = options.top;
-      top.style.backgroundColor = color;
-    }
-    if (right !== null) {
-      right.style.width = options.right;
-      right.style.backgroundColor = color;
-    }
-    if (bottom !== null) {
-      bottom.style.height = options.bottom;
-      bottom.style.backgroundColor = color;
-    }
-    if (left !== null) {
-      left.style.width = options.left;
-      left.style.backgroundColor = color;
-    }
-    if (container !== null) {
-      container.style.borderWidth = options.container;
-    }
-    if (footer !== null) {
-      footer.style.backgroundColor = color;
-    }
-    if (footerCopyright !== null) {
-      const isLight = isLightColorTone(color);
-      const colorValue = isLight ? '#424242' : '#eee';
-      footerCopyright.style.color = colorValue;
-    }
-    return () => {
-      if (top !== null) {
-        top.style.height = '2px';
-        top.style.backgroundColor = '#c4e0fa';
-      }
-      if (right !== null) {
-        right.style.width = '2px';
-        right.style.backgroundColor = '#c4e0fa';
-      }
-      if (bottom !== null) {
-        bottom.style.height = '2px';
-        bottom.style.backgroundColor = '#c4e0fa';
-      }
-      if (left !== null) {
-        left.style.width = '2px';
-        left.style.backgroundColor = '#c4e0fa';
-      }
-      if (container !== null) {
-        container.style.borderWidth = '2px';
-      }
-      if (footer !== null) {
-        footer.style.backgroundColor = '#c4e0fa';
-      }
-      if (footerCopyright !== null) {
-        footerCopyright.style.color = '#424242';
-      }
-    };
-    // eslint-disable-next-line
-  }, []);
+	useEffect(() => {
+		const top = document.getElementById("top");
+		const right = document.getElementById("right");
+		const bottom = document.getElementById("bottom");
+		const left = document.getElementById("left");
+		const container = document.getElementById("container");
+		const footer = document.getElementById("footer");
+		const footerCopyright = document.getElementById("footer-copyright");
+		if (top !== null) {
+			top.style.height = options.top;
+			top.style.backgroundColor = color;
+		}
+		if (right !== null) {
+			right.style.width = options.right;
+			right.style.backgroundColor = color;
+		}
+		if (bottom !== null) {
+			bottom.style.height = options.bottom;
+			bottom.style.backgroundColor = color;
+		}
+		if (left !== null) {
+			left.style.width = options.left;
+			left.style.backgroundColor = color;
+		}
+		if (container !== null) {
+			container.style.borderWidth = options.container;
+		}
+		if (footer !== null) {
+			footer.style.backgroundColor = color;
+		}
+		if (footerCopyright !== null) {
+			const isLight = isLightColorTone(color);
+			const colorValue = isLight ? "#424242" : "#eee";
+			footerCopyright.style.color = colorValue;
+		}
+		return () => {
+			if (top !== null) {
+				top.style.height = "2px";
+				top.style.backgroundColor = "#c4e0fa";
+			}
+			if (right !== null) {
+				right.style.width = "2px";
+				right.style.backgroundColor = "#c4e0fa";
+			}
+			if (bottom !== null) {
+				bottom.style.height = "2px";
+				bottom.style.backgroundColor = "#c4e0fa";
+			}
+			if (left !== null) {
+				left.style.width = "2px";
+				left.style.backgroundColor = "#c4e0fa";
+			}
+			if (container !== null) {
+				container.style.borderWidth = "2px";
+			}
+			if (footer !== null) {
+				footer.style.backgroundColor = "#c4e0fa";
+			}
+			if (footerCopyright !== null) {
+				footerCopyright.style.color = "#424242";
+			}
+		};
+		// eslint-disable-next-line
+	}, []);
 }
 
 /**
  * body 태그에 stop-scrolling 클래스를 부여합니다.
  */
 export function useStopBodyScroll() {
-  useEffect(() => {
-    document.body.classList.add('stop-scrolling');
-    return () => {
-      document.body.classList.remove('stop-scrolling');
-    };
-  }, []);
+	useEffect(() => {
+		document.body.classList.add("stop-scrolling");
+		return () => {
+			document.body.classList.remove("stop-scrolling");
+		};
+	}, []);
 }
 
 /**
@@ -127,24 +127,24 @@ export function useStopBodyScroll() {
  * @returns nothing
  */
 export function useBodyColor(color: String) {
-  useEffect(() => {
-    document.body.style.transition = 'background-color 0.5s';
-    document.body.style.backgroundColor = `${color}`;
-    return () => {
-      document.body.style.backgroundColor = '#fff';
-    };
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
+	useEffect(() => {
+		document.body.style.transition = "background-color 0.5s";
+		document.body.style.backgroundColor = `${color}`;
+		return () => {
+			document.body.style.backgroundColor = "#fff";
+		};
+		// eslint-disable-next-line react-hooks/exhaustive-deps
+	}, []);
 }
 
 /**
  * Octokit 클라이언트 셋업입니다. Public 권한으로 실행되는 octokit입니다.
  */
 export const octokitForBlog = new Octokit({
-  auth:
-    // process.env.REACT_APP_PERSONAL_TOKEN
-    // process.env.GITHUB_TOKEN,
-    '',
+	auth:
+		// process.env.REACT_APP_PERSONAL_TOKEN
+		// process.env.GITHUB_TOKEN,
+		"",
 });
 
 /**
@@ -152,10 +152,10 @@ export const octokitForBlog = new Octokit({
  * @returns issues 탭 정보를 담은 Promise 객체
  */
 export function getIssues() {
-  return octokitForBlog.request('GET /repos/{owner}/{repo}/issues', {
-    owner: 'Citysquirrel',
-    repo: 'citysquirrel.github.io',
-  });
+	return octokitForBlog.request("GET /repos/{owner}/{repo}/issues", {
+		owner: "Citysquirrel",
+		repo: "citysquirrel.github.io",
+	});
 }
 
 /**
@@ -163,9 +163,7 @@ export function getIssues() {
  * {@link https://docs.github.com/en/developers/apps/building-oauth-apps/authorizing-oauth-apps}
  */
 export function execGithubLogin() {
-  window.location.assign(
-    'https://github.com/login/oauth/authorize?client_id=dbbd22f1eae2c8421188'
-  );
+	window.location.assign("https://github.com/login/oauth/authorize?client_id=dbbd22f1eae2c8421188");
 }
 
 /**
@@ -173,12 +171,12 @@ export function execGithubLogin() {
  * {@link https://docs.github.com/en/rest/users/users#get-the-authenticated-user}
  */
 export function getUserInfo() {
-  const octokit = new Octokit({
-    auth: 'YOUR-TOKEN',
-  });
-  octokit.request('GET /user', {});
-  //Authorization: Bearer OAUTH-TOKEN
-  // axios.get("https://api.github.com/user",{headers: {authorization:`Bearer ${token}` }})
+	const octokit = new Octokit({
+		auth: "YOUR-TOKEN",
+	});
+	octokit.request("GET /user", {});
+	//Authorization: Bearer OAUTH-TOKEN
+	// axios.get("https://api.github.com/user",{headers: {authorization:`Bearer ${token}` }})
 }
 
 /**
@@ -187,7 +185,7 @@ export function getUserInfo() {
  * @returns HTML 형식의 문자열을 담은 Promise
  */
 export async function renderMarkdown(text: string) {
-  return await octokitForBlog.request('POST /markdown', { text });
+	return await octokitForBlog.request("POST /markdown", { text });
 }
 
 /**
@@ -196,39 +194,39 @@ export async function renderMarkdown(text: string) {
  * @returns `{ kr: 한글형태의 로컬시간, alias: 작성시점으로부터의 시간차 }`
  */
 export const modifyDatetime = (datetime: string | undefined) => {
-  // datetime: UTC 시간
-  if (datetime === undefined) return { kr: undefined, alias: undefined };
-  const received = new Date(datetime);
-  const now = new Date();
-  const nowYear = now.getFullYear();
-  const nowMonth = now.getMonth();
-  const nowDay = now.getDate();
-  const nowHour = now.getHours();
-  const nowMin = now.getMinutes();
+	// datetime: UTC 시간
+	if (datetime === undefined) return { kr: undefined, alias: undefined };
+	const received = new Date(datetime);
+	const now = new Date();
+	const nowYear = now.getFullYear();
+	const nowMonth = now.getMonth();
+	const nowDay = now.getDate();
+	const nowHour = now.getHours();
+	const nowMin = now.getMinutes();
 
-  // 몇분전, 몇시간전, 몇일전, 몇달전, 몇년전
-  // 1분 한시간 하루 한달 일년
-  const aMinuteAgo = new Date(nowYear, nowMonth, nowDay, nowHour, nowMin - 1);
-  const aHourAgo = new Date(nowYear, nowMonth, nowDay, nowHour - 1, nowMin);
-  const aDayAgo = new Date(nowYear, nowMonth, nowDay - 1, nowHour, nowMin);
-  const aMonthAgo = new Date(nowYear, nowMonth - 1, nowDay, nowHour, nowMin);
-  const aYearAgo = new Date(nowYear - 1, nowMonth, nowDay, nowHour, nowMin);
-  const difference = now.getTime() - received.getTime();
-  let alias;
-  if (aMinuteAgo <= received) {
-    alias = '방금 전';
-  } else if (aHourAgo <= received) {
-    alias = `${Math.ceil(difference / 1000 / 60)}분 전`;
-  } else if (aDayAgo <= received) {
-    alias = `${Math.ceil(difference / 1000 / 60 / 60)}시간 전`;
-  } else if (aMonthAgo <= received) {
-    alias = `${Math.ceil(difference / 1000 / 60 / 60 / 24)}일 전`;
-  } else if (aYearAgo <= received) {
-    alias = `${Math.ceil(difference / 1000 / 60 / 60 / 24 / 30)}개월 전`;
-  } else {
-    alias = `${Math.ceil(difference / 1000 / 60 / 60 / 24 / 30 / 12)}년 전`;
-  }
-  return { kr: received.toLocaleString('KR'), alias };
+	// 몇분전, 몇시간전, 몇일전, 몇달전, 몇년전
+	// 1분 한시간 하루 한달 일년
+	const aMinuteAgo = new Date(nowYear, nowMonth, nowDay, nowHour, nowMin - 1);
+	const aHourAgo = new Date(nowYear, nowMonth, nowDay, nowHour - 1, nowMin);
+	const aDayAgo = new Date(nowYear, nowMonth, nowDay - 1, nowHour, nowMin);
+	const aMonthAgo = new Date(nowYear, nowMonth - 1, nowDay, nowHour, nowMin);
+	const aYearAgo = new Date(nowYear - 1, nowMonth, nowDay, nowHour, nowMin);
+	const difference = now.getTime() - received.getTime();
+	let alias;
+	if (aMinuteAgo <= received) {
+		alias = "방금 전";
+	} else if (aHourAgo < received) {
+		alias = `${Math.ceil(difference / 1000 / 60)}분 전`;
+	} else if (aDayAgo < received) {
+		alias = `${Math.ceil(difference / 1000 / 60 / 60)}시간 전`;
+	} else if (aMonthAgo < received) {
+		alias = `${Math.ceil(difference / 1000 / 60 / 60 / 24)}일 전`;
+	} else if (aYearAgo < received) {
+		alias = `${Math.ceil(difference / 1000 / 60 / 60 / 24 / 30)}개월 전`;
+	} else {
+		alias = `${Math.ceil(difference / 1000 / 60 / 60 / 24 / 30 / 12)}년 전`;
+	}
+	return { kr: received.toLocaleString("KR"), alias };
 };
 
 /**
@@ -236,16 +234,16 @@ export const modifyDatetime = (datetime: string | undefined) => {
  * @param state 임의의 변수
  * @returns 변수의 이전 상태
  */
-export function useConsole(state: any, name = 'console') {
-  useEffect(() => {
-    console.log(`${name}: `, state);
-    // eslint-disable-next-line
-  }, [state]);
-  return state;
+export function useConsole(state: any, name = "console") {
+	useEffect(() => {
+		console.log(`${name}: `, state);
+		// eslint-disable-next-line
+	}, [state]);
+	return state;
 }
 
 interface IQueryObject {
-  [key: string]: string;
+	[key: string]: string;
 }
 
 /**
@@ -254,18 +252,18 @@ interface IQueryObject {
  * @returns `key-value Object`
  */
 export function modQueryString(str: string | undefined) {
-  const decoded = typeof str === 'string' ? decodeURI(str) : '';
-  const array = decoded
-    .slice(1)
-    .split('&')
-    .map((query) => query.split('='));
+	const decoded = typeof str === "string" ? decodeURI(str) : "";
+	const array = decoded
+		.slice(1)
+		.split("&")
+		.map((query) => query.split("="));
 
-  const object = array.reduce<IQueryObject>((t, [k, v], i) => {
-    t[k] = v;
-    return t;
-  }, {});
+	const object = array.reduce<IQueryObject>((t, [k, v], i) => {
+		t[k] = v;
+		return t;
+	}, {});
 
-  return object;
+	return object;
 }
 
 /**
@@ -273,32 +271,32 @@ export function modQueryString(str: string | undefined) {
  * @param def 의존성 배열
  */
 export const useLabelAllCheckbox = (def: any = null) => {
-  useEffect(() => {
-    const inputs = document.querySelectorAll('input');
+	useEffect(() => {
+		const inputs = document.querySelectorAll("input");
 
-    inputs.forEach((input, index) => {
-      input.setAttribute('id', `check${index}`);
-      const label = document.createElement('label');
-      label.setAttribute('for', `check${index}`);
-      input.insertAdjacentElement('afterend', label);
-    });
-  }, [def]);
+		inputs.forEach((input, index) => {
+			input.setAttribute("id", `check${index}`);
+			const label = document.createElement("label");
+			label.setAttribute("for", `check${index}`);
+			input.insertAdjacentElement("afterend", label);
+		});
+	}, [def]);
 };
 
 export const useCopyAllCodeblock = (def: any = null) => {
-  const Button = styled.button``;
-  useEffect(() => {
-    document.querySelectorAll('pre').forEach((pre) => {
-      const code = pre.childNodes[0];
-      const handleClick = (e: React.MouseEvent) => {
-        navigator.clipboard.writeText(code.textContent!);
-      };
-      const jsx = (
-        <Button onClick={handleClick}>
-          <FaRegCopy />
-        </Button>
-      );
-    });
-    // eslint-disable-next-line
-  }, [def]);
+	const Button = styled.button``;
+	useEffect(() => {
+		document.querySelectorAll("pre").forEach((pre) => {
+			const code = pre.childNodes[0];
+			const handleClick = (e: React.MouseEvent) => {
+				navigator.clipboard.writeText(code.textContent!);
+			};
+			const jsx = (
+				<Button onClick={handleClick}>
+					<FaRegCopy />
+				</Button>
+			);
+		});
+		// eslint-disable-next-line
+	}, [def]);
 };
--- src/lib/MarkdownConfig.tsx
+++ src/lib/MarkdownConfig.tsx
@@ -1,50 +1,45 @@
-import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
-import { coldarkCold as style } from 'react-syntax-highlighter/dist/esm/styles/prism';
+import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
+import { coldarkCold as style } from "react-syntax-highlighter/dist/esm/styles/prism";
 
 interface ICode {
-  className: string | undefined;
-  children: string | string[];
+	className: string | undefined;
+	children: string | string[];
 }
 
 interface IPre {
-  children: JSX.Element;
+	children: JSX.Element;
 }
 
 const CodeConfig = ({ className, children }: ICode) => {
-  const myStyle = {
-    borderRadius: '4px',
-    padding: '16px',
-    margin: '8px 0',
-    backgroundColor: 'var(--soft-blue-100)',
-  };
-  let language = 'text';
-  if (className && className.startsWith('lang-')) {
-    language = className.replace('lang-', '');
-  }
-  return (
-    <>
-      {/* 복사 기능을 위한 보조 요소 추가 */}
-      <div></div>
-      <SyntaxHighlighter
-        language={language}
-        style={style}
-        customStyle={myStyle}
-        wrapLines={true}
-        children={children}
-      />
-    </>
-  );
+	const myStyle = {
+		borderRadius: "4px",
+		padding: "16px",
+		margin: "8px 0",
+		backgroundColor: "var(--soft-blue-100)",
+	};
+	let language = "text";
+	if (className && className.startsWith("lang-")) {
+		language = className.replace("lang-", "");
+	}
+
+	return (
+		<>
+			{/* 복사 기능을 위한 보조 요소 추가 */}
+			<div></div>
+			<SyntaxHighlighter language={language} style={style} customStyle={myStyle} wrapLines={true} children={children} />
+		</>
+	);
 };
 
 const PreConfig = ({ children, ...rest }: IPre) => {
-  if ('type' in children && children['type'] === 'code') {
-    return CodeConfig(children['props']);
-  }
-  return (
-    <pre style={{ borderRadius: '4px' }} {...rest}>
-      {children}
-    </pre>
-  );
+	if ("type" in children && children["type"] === "code") {
+		return CodeConfig(children["props"]);
+	}
+	return (
+		<pre style={{ borderRadius: "4px" }} {...rest}>
+			{children}
+		</pre>
+	);
 };
 
 export default PreConfig;
